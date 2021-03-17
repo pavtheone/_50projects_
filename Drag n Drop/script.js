95cfc53360ba23362793f7cd3c98f1ed6fe7b350
pavtheone <whiterabbit.webdesign89@gmail.com>
2021-03-17 19:10:34 +0100

    Initial commit

---
+++ Drag n Drop/script.js
@@ -0,0 +1,27 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+
+function dragStart() {
+    console.log('drag start')
+}
+
+function dragEnd() {
+    console.log('drag end')
+}
+
+function dragOver() {
+    console.log('drag over')
+}
+
+function dragEnter() {
+    console.log('drag enter')
+}
+
+function dragLeave() {
+    console.log('drag leave')
+}
+
+function dragDrop() {
+    console.log('drag drop')
+}
